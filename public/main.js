162136b915d526aef75a8b3f3669463561b68f5c
Mikal910 <mikalmuwakkil@gmail.com>
2024-11-17 17:57:23 -0500

    Justice

--- public/main.js
+++ public/main.js
@@ -1,45 +1,67 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+const moveToRead = document.getElementsByClassName("move-to-read");
+const moveToWant = document.getElementsByClassName("move-to-want");
+const trash = document.getElementsByClassName("fa-trash");
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
+Array.from(moveToRead).forEach(function(element) {
+    element.addEventListener('click', function() {
+        const title = this.parentNode.childNodes[1].innerText;
+        const author = this.parentNode.childNodes[3].innerText;
+        console.log(title,author)
+        fetch('comics/move-to-read', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'title': title,
+                'author': author
+            })
         })
         .then(response => {
-          if (response.ok) return response.json()
+            if (response.ok) return response.json();
         })
         .then(data => {
-          console.log(data)
-          window.location.reload(true)
+            console.log(data);
+            window.location.reload(true);
+        });
+    });
+});
+
+Array.from(moveToWant).forEach(function(element) {
+    element.addEventListener('click', function() {
+        const title = this.parentNode.childNodes[1].innerText;
+        const author = this.parentNode.childNodes[3].innerText;
+        fetch('comics/move-to-want', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'title': title,
+                'author': author
+            })
         })
-      });
+        .then(response => {
+            if (response.ok) return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            window.location.reload(true);
+        });
+    });
 });
 
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+    element.addEventListener('click', function() {
+        const title = this.parentNode.parentNode.childNodes[1].innerText;
+        const author = this.parentNode.parentNode.childNodes[3].innerText;
+        fetch('comics', {
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'title': title,
+                'author': author
+            })
+        }).then(function(response) {
+            window.location.reload();
+        });
+    });
 });
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url = 'mongodb+srv://DEMO:DEMO@cluster0.ulmcg.mongodb.net/VideoGames?retryWrites=true&w=majority&appName=Cluster0';
+const dbName = "VideoGames";
 
 app.listen(3000, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-        if(error) {
+        if (error) {
             throw error;
         }
         db = client.db(dbName);
@@ -18,44 +18,74 @@ app.listen(3000, () => {
     });
 });
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
-  })
-})
+    db.collection('Games').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        res.render('index.ejs', { comics: result });
+    });
+});
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+app.post('/comics/add', (req, res) => {
+    db.collection('Games').insertOne(
+        { title: req.body.title, author: req.body.author, read:false},
+        (err, result) => {
+            if (err) return console.log(err);
+            console.log('saved to database');
+            res.redirect('/');
+        }
+    );
+});
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+app.put('/comics/move-to-read', (req, res) => {
+    db.collection('Games').findOneAndUpdate(
+        { title: req.body.title, author: req.body.author },
+        {
+            $set: {
+                read: true
+            }
+        },
+        {
+            sort: { _id: -1 },
+            upsert: true
+        },
+        (err, result) => {
+            if (err) return res.send(err);
+            res.send(result);
+        }
+    );
+});
 
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('Message deleted!')
-  })
-})
+app.put('/comics/move-to-want', (req, res) => {
+    db.collection('Games').findOneAndUpdate(
+        { title: req.body.title, author: req.body.author },
+        {
+            $set: {
+                read: false
+            }
+        },
+        {
+            sort: { _id: -1 },
+            upsert: true
+        },
+        (err, result) => {
+            if (err) return res.send(err);
+            res.send(result);
+        }
+    );
+});
+
+app.delete('/comics', (req, res) => {
+    db.collection('Games').findOneAndDelete(
+        { title: req.body.name, author: req.body.msg },
+        (err, result) => {
+            if (err) return res.send(500, err);
+            res.send('Message deleted!');
+        }
+    );
+});
+
